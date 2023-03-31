99b73e41c5ac790d6e9932d89a5eee1c9ffb893e
Elea Pauliat <122291811+epauliat@users.noreply.github.com>
2023-03-31 15:40:15 +0200

    Cleaned and Organized the code

--- Fusion.py
+++ Fusion.py
@@ -1,59 +1,74 @@
-
 from autoencoder import *
 from geneticAlgo import *
 
-def mutation(num):
-    loaded_decoder=load_decoder("models/decoder_18_03_15epochs_256batchsize.pt")
-    loaded_encoder=load_encoder("models/encoder_18_03_15epochs_256batchsize.pt")
-    vect_select=[]
-    for i in range(num):
-        vect_select.append(encoding_Image_to_Vector("temp/"+str(i)+".jpg",loaded_encoder))
-    new_vectors=allNewvectors(vect_select,1)
-    for i, vector in enumerate(new_vectors):
-        decoded_pil=decoding_Vector_to_Image(vector,loaded_decoder)
-        decoded_pil.save("images/"+str(i)+".PNG", format="png")
+def mutating(encoded_vectors,std_file_path, show):
+    """Functions that mutates the encoded images and prints them
+        Args:
+            encoded_vectors (list): list of encoded vectors to be mutated
+            std_file_path (str): path to the std txt file
+            show (boolean): if True, mutated vectors are shown in a pop-up
+        Returns:
+            list: list of encoded mutated images
 
+    """
+    # LOADING THE STDS PER POSITION FROM TEXT FILE
+
+    std = []
+    with open(std_file_path) as f:
+        std = f.readlines()
+    stds = std[0].split(' ')
+
+    for i in range(len(stds)):
+        stds[i]=float(stds[i])
+
+    # MUTATING ENCODED VECTORS
+
+    new_vectors=allNewvectors(vect_select,1.6,stds)
+
+    # PLOTTING MUTATED SOLUTIONS
+
+    if(show == True):
+        fig = plt.figure(figsize=(100,100))
+        for i, vector in enumerate(new_vectors):
+            ax = fig.add_subplot(1,10,i+1)
+            decoded_pil=decoding_Vector_to_Image(vector,loaded_decoder)
+            ax.imshow(decoded_pil)
+            decoded_pil.save(str(i)+".png", format="png")
+        plt.show()
+
+    return new_vectors
 
 
 if __name__ == "__main__":
 
-    loaded_decoder=load_decoder("models/decoder_18_03_15epochs_256batchsize.pt")
-    loaded_encoder=load_encoder("models/encoder_18_03_15epochs_256batchsize.pt")
-    encoded_image1 = encoding_Image_to_Vector('faces/Afton_Smith/Afton_Smith_0001.jpg',loaded_encoder)
-    encoded_image2 = encoding_Image_to_Vector("faces/Azra_Akin/Azra_Akin_0001.jpg",loaded_encoder)
-    encoded_image3 = encoding_Image_to_Vector("faces/Aaron_Patterson/Aaron_Patterson_0001.jpg",loaded_encoder)
-    # decoding_images(loaded_decoder,loaded_encoder,"faces/Afton_Smith/Afton_Smith_0001.jpg")
-    # decoding_images(loaded_decoder,loaded_encoder,"faces/Azra_Akin/Azra_Akin_0001.jpg")
-    # decoding_images(loaded_decoder,loaded_encoder,"faces/Aaron_Patterson/Aaron_Patterson_0001.jpg")
+    # LOADING MODELS
+
+    loaded_decoder=load_decoder("models/BIS70000decoder_31_03_12epoch_16batchsize_64size_stride_padding.pt")
+    loaded_encoder=load_encoder("models/BIS70000encoder_31_03_12epoch_16batchsize_64size_stride_padding.pt")
+
+    # ENCODING TEST IMAGES
+
+    encoded_image1 = encoding_Image_to_Vector("faces_bis/43000/43002.png",loaded_encoder)
+    encoded_image2 = encoding_Image_to_Vector("faces_bis/43000/43011.png",loaded_encoder)
+    encoded_image3 = encoding_Image_to_Vector("faces_bis/43000/43020.png",loaded_encoder)
 
     vect_select=[encoded_image1,encoded_image2,encoded_image3]
 
+    # DECODING IMAGES
+
+    decoding_images(loaded_encoder,loaded_decoder,"faces_bis/43000/43002.png")
+    decoding_images(loaded_encoder,loaded_decoder,"faces_bis/43000/43011.png")
+    decoding_images(loaded_encoder,loaded_decoder,"faces_bis/43000/43020.png")
+
+    # PLOTTING DECODED VECTORS
+
     fig = plt.figure(figsize=(100,100))
     for i, vector in enumerate(vect_select):
-        ax = fig.add_subplot(1,3,i+1)
+        ax = fig.add_subplot(1,len(vect_select),i+1)
         decoded_pil=decoding_Vector_to_Image(vector,loaded_decoder)
         ax.imshow(decoded_pil)
         decoded_pil.save(str(i)+".png", format="png")
     plt.show()
 
 
-    new_vectors=allNewvectors(vect_select,1)
-    print(len(new_vectors[0]))
-    #mutation only
-    fig = plt.figure(figsize=(100,100))
-    for i, vector in enumerate(new_vectors):
-        ax = fig.add_subplot(1,10,i+1)
-        decoded_pil=decoding_Vector_to_Image(vector,loaded_decoder)
-        ax.imshow(decoded_pil)
-        decoded_pil.save(str(i)+".png", format="png")
-    plt.show()
-
-    # #crossing over
-    # fig = plt.figure(figsize=(100,100))
-    # mut2 = multi_point_crossover(list_mutated_vector)
-    # for i, vector in enumerate(mut2):
-    #     ax = fig.add_subplot(1,5,i+1)
-    #     decoded_pil=decoding_Vector(vector,loaded_decoder)
-    #     ax.imshow(decoded_pil)
-    #     decoded_pil.save("0"+str(i)+".png", format="png")
-    # plt.show()
+    mutating(vect_select,"stds_of_all_encoded_vector_per_position.txt", show=True)
