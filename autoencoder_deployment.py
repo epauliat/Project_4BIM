807387cf0e50d25a013525fbb0911a8c484c603c
Elea Pauliat <122291811+epauliat@users.noreply.github.com>
2023-03-31 17:16:11 +0200

    Deployment version of autoencoder
    - Commented the data import and training parts of the code so that we don't need to install the packages used in training.
    - Less dependencies.
    - Easier and faster deployment.

--- autoencoder_deployment.py
+++ autoencoder_deployment.py
@@ -4,7 +4,7 @@
 
 import os
 import sys
-import time
+# import time
 import torch
 
 import numpy as np
@@ -13,42 +13,40 @@ import torch.nn as nn
 from PIL import Image
 from math import sqrt
 from statistics import mean
-from torchsummary import summary
 from matplotlib import pyplot as plt
 from matplotlib import image as mpimg
 from torchvision import transforms, datasets
-from keras_preprocessing.image import img_to_array
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
 
 ###################
 #  DATA & IMAGES  #
 ###################
 
-def Data_import(dataset_path, batchsize):
-    """Function that imports all the images in a given directory as a DataLoader
-        Args:
-            dataset_path (str): directory path to images
-            batchsize (int): size of batches created by the DataLoader
-        Returns:
-            DataLoader: object containing all the imported images in batches
-    """
-    dataset = datasets.ImageFolder(root=dataset_path, transform=transforms.Compose([transforms.ToTensor(),
-
-                                                                                    transforms.Resize((64,64))])) #transforms.CenterCrop(200),
-
-    X_train, X_validation = train_test_split(dataset,test_size=0.2, random_state=1) # add shuffle option = True ?
-
-    train_dataloader = torch.utils.data.DataLoader(X_train, batch_size = batchsize, shuffle = True)
-    print("Training data acquired")
-    print(f"Training dataloader contains : {len(train_dataloader)} batchs each containing {batchsize} images")
-
-    valid_dataloader = torch.utils.data.DataLoader(X_validation, batch_size = batchsize, shuffle = False)
-    print("Validation data acquired")
-    print(f"Training dataloader contains : {len(valid_dataloader)} batchs each containing {batchsize} images")
-
-
-    return train_dataloader,valid_dataloader
+# def Data_import(dataset_path, batchsize):
+#     """Function that imports all the images in a given directory as a DataLoader
+#         Args:
+#             dataset_path (str): directory path to images
+#             batchsize (int): size of batches created by the DataLoader
+#         Returns:
+#             DataLoader: object containing all the imported images in batches
+#     """
+#     dataset = datasets.ImageFolder(root=dataset_path, transform=transforms.Compose([transforms.ToTensor(),
+#
+#                                                                                     transforms.Resize((64,64))])) #transforms.CenterCrop(200),
+#
+#     X_train, X_validation = train_test_split(dataset,test_size=0.2, random_state=1) # add shuffle option = True ?
+#
+#     train_dataloader = torch.utils.data.DataLoader(X_train, batch_size = batchsize, shuffle = True)
+#     print("Training data acquired")
+#     print(f"Training dataloader contains : {len(train_dataloader)} batchs each containing {batchsize} images")
+#
+#     valid_dataloader = torch.utils.data.DataLoader(X_validation, batch_size = batchsize, shuffle = False)
+#     print("Validation data acquired")
+#     print(f"Training dataloader contains : {len(valid_dataloader)} batchs each containing {batchsize} images")
+#
+#
+#     return train_dataloader,valid_dataloader
 
 def Image_Conversion_to_tensor(path):
     """Function that converts an imported image in PIL format to a Tensor
@@ -178,92 +176,92 @@ class Autoencoder(nn.Module):
         decoded = self.decoder(encoded)
         return decoded
 
-def train(nb_epoch, train_data, valid_data, autoencoder):
-    """Function that trains an autoencoder on a given dataset
-        Args:
-            nb_epoch (int): number of epochs to train the model with
-            train_data (DataLoader): imported images in batches
-            data_data (DataLoader): imported images for validation in batches
-            autoencoder (Autoencoder): autoencoder model to train
-        Returns:
-            list: list of losses per epoch
-            list: list of losses per epoch for validation set
-    """
-    # Loss and optimizer definition
-    criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(autoencoder.parameters(), lr=1e-3, weight_decay=1e-5)
+# def train(nb_epoch, train_data, valid_data, autoencoder):
+#     """Function that trains an autoencoder on a given dataset
+#         Args:
+#             nb_epoch (int): number of epochs to train the model with
+#             train_data (DataLoader): imported images in batches
+#             data_data (DataLoader): imported images for validation in batches
+#             autoencoder (Autoencoder): autoencoder model to train
+#         Returns:
+#             list: list of losses per epoch
+#             list: list of losses per epoch for validation set
+#     """
+#     # Loss and optimizer definition
+#     criterion = nn.MSELoss()
+#     optimizer = torch.optim.Adam(autoencoder.parameters(), lr=1e-3, weight_decay=1e-5)
+#
+#     # Output lists
+#     train_losses = []
+#     valid_losses = []
+#
+#     i=0
+#     for epoch in range(1,nb_epoch+1):
+#         tic = time.time()
+#
+#         nb_train_batch = len(train_data)
+#         nb_valid_batch = len(valid_data)
+#
+#         # --- Training ---
+#         train_loss = 0
+#         print("Training")
+#         for (index,batch) in enumerate(train_data):
+#             print("batch ", index, "/",nb_train_batch, end=', ')
+#             l = 0
+#             for j in range(len(batch[0])):
+#                 img=batch[0][j]
+#                 batchsize=len(batch[0])
+#                 img = img.reshape(1, 3, 64, 64)
+#                 recon=autoencoder(img)
+#                 l += criterion(recon, img)
+#
+#             l.backward()
+#             optimizer.step()
+#             optimizer.zero_grad()
+#             train_loss += float(l)
+#
+#         train_losses.append(train_loss/nb_train_batch)
+#
+#         # --- Validation ---
+#         valid_loss = 0
+#         with torch.no_grad():
+#             print("Validation")
+#             for (index,batch) in enumerate(valid_data):
+#                 print("batch ", index, "/",nb_valid_batch, end=', ')
+#                 l = 0
+#                 for j in range(len(batch[0])):
+#                     img=batch[0][j]
+#                     batchsize=len(batch[0])
+#                     img = img.reshape(1, 3, 64, 64)
+#                     recon=autoencoder(img)
+#                     l += float(criterion(recon, img))
+#
+#                 valid_loss += l
+#
+#             valid_losses.append(valid_loss/nb_valid_batch)
+#
+#         toc = time.time()
+#         print(f"Epoch {epoch}/{nb_epoch}: training loss = {train_losses[-1]:.5f}, val loss={valid_losses[-1] :.5f}, Training time : {toc-tic:.2f} s")
+#
+#     return train_losses,valid_losses
 
-    # Output lists
-    train_losses = []
-    valid_losses = []
-
-    i=0
-    for epoch in range(1,nb_epoch+1):
-        tic = time.time()
-
-        nb_train_batch = len(train_data)
-        nb_valid_batch = len(valid_data)
-
-        # --- Training ---
-        train_loss = 0
-        print("Training")
-        for (index,batch) in enumerate(train_data):
-            print("batch ", index, "/",nb_train_batch, end=', ')
-            l = 0
-            for j in range(len(batch[0])):
-                img=batch[0][j]
-                batchsize=len(batch[0])
-                img = img.reshape(1, 3, 64, 64)
-                recon=autoencoder(img)
-                l += criterion(recon, img)
-
-            l.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-            train_loss += float(l)
-
-        train_losses.append(train_loss/nb_train_batch)
-
-        # --- Validation ---
-        valid_loss = 0
-        with torch.no_grad():
-            print("Validation")
-            for (index,batch) in enumerate(valid_data):
-                print("batch ", index, "/",nb_valid_batch, end=', ')
-                l = 0
-                for j in range(len(batch[0])):
-                    img=batch[0][j]
-                    batchsize=len(batch[0])
-                    img = img.reshape(1, 3, 64, 64)
-                    recon=autoencoder(img)
-                    l += float(criterion(recon, img))
-
-                valid_loss += l
-
-            valid_losses.append(valid_loss/nb_valid_batch)
-
-        toc = time.time()
-        print(f"Epoch {epoch}/{nb_epoch}: training loss = {train_losses[-1]:.5f}, val loss={valid_losses[-1] :.5f}, Training time : {toc-tic:.2f} s")
-
-    return train_losses,valid_losses
-
-def plot_Losses_Curve(losses_curve_list,losses_curve_valid_list):
-    """Function that plots losses per epoch
-        Args:
-            losses_curve_list (list): loss per epoch
-            losses_curve_valid_list (list): loss per epoch for validation set
-        Returns:
-            None
-    """
-    fig, ax1 = plt.subplots()
-    ax1.set_xlabel("epoch")
-    ax1.set_ylabel("loss")
-    ax1.plot(losses_curve_list, "r")
-    ax1.plot(losses_curve_valid_list, "b")
-    plt.xticks(np.arange(len(losses_curve_list)), np.arange(1, len(losses_curve_list)+1))
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
-    ax1.set_title("Loss per epoch")
-    plt.show()
+# def plot_Losses_Curve(losses_curve_list,losses_curve_valid_list):
+#     """Function that plots losses per epoch
+#         Args:
+#             losses_curve_list (list): loss per epoch
+#             losses_curve_valid_list (list): loss per epoch for validation set
+#         Returns:
+#             None
+#     """
+#     fig, ax1 = plt.subplots()
+#     ax1.set_xlabel("epoch")
+#     ax1.set_ylabel("loss")
+#     ax1.plot(losses_curve_list, "r")
+#     ax1.plot(losses_curve_valid_list, "b")
+#     plt.xticks(np.arange(len(losses_curve_list)), np.arange(1, len(losses_curve_list)+1))
+#     fig.tight_layout()  # otherwise the right y-label is slightly clipped
+#     ax1.set_title("Loss per epoch")
+#     plt.show()
 
 
 ######################
@@ -271,32 +269,32 @@ def plot_Losses_Curve(losses_curve_list,losses_curve_valid_list):
 #  SAVING & LOADING  #
 ######################
 
-def save(model, path):
-    """Function that saves an Autoencoder model to a file
-        Args:
-            model (Autoencoder): the model to save
-        Returns:
-            None
-    """
-    torch.save(model.state_dict(), path)
-
-def save_decoder(model, path):
-    """Function that saves a Decoder model to a file
-        Args:
-            model (Autoencoder): model having a Decoder model as attribute
-        Returns:
-            None
-    """
-    torch.save(model.decoder.state_dict(), path)
-
-def save_encoder(model, path):
-    """Function that saves an Encoder model to a file
-        Args:
-            model (Autoencoder): model having an Encoder model as attribute
-        Returns:
-            None
-    """
-    torch.save(model.encoder.state_dict(), path)
+# def save(model, path):
+#     """Function that saves an Autoencoder model to a file
+#         Args:
+#             model (Autoencoder): the model to save
+#         Returns:
+#             None
+#     """
+#     torch.save(model.state_dict(), path)
+#
+# def save_decoder(model, path):
+#     """Function that saves a Decoder model to a file
+#         Args:
+#             model (Autoencoder): model having a Decoder model as attribute
+#         Returns:
+#             None
+#     """
+#     torch.save(model.decoder.state_dict(), path)
+#
+# def save_encoder(model, path):
+#     """Function that saves an Encoder model to a file
+#         Args:
+#             model (Autoencoder): model having an Encoder model as attribute
+#         Returns:
+#             None
+#     """
+#     torch.save(model.encoder.state_dict(), path)
 
 def load_autoencoder(path):
     """Function that loads an Autoencoder model from a given file
@@ -339,34 +337,34 @@ def load_encoder(path):
 #  ALL IN ONE MODEL  #
 #       METHOD       #
 ######################
-
-def training_Saving_Autoencoder(nb_epoch, batchsize, dataset_path, saving_path, saving_path_decoder,saving_path_encoder):
-    """Function that creates an autoencoder, trains it, and saves it to a file
-        Args:
-            nb_epoch (int): number of epoch to train the model with
-            batchsize (int): batch size of data
-            saving_path (str): path to the file in which the autoencoder is saved
-            dataset_path (str): path to a directory containing the data as images
-            saving_path_decoder (str): path to the file in which the decoder is saved
-            saving_path_encoder (str): path to the file in which the encoder is saved
-        Returns:
-            Autoencoder: the created and trained autoencoder
-    """
-    print("Importing data ...")
-    train_data, valid_data = Data_import(dataset_path, batchsize)
-
-    my_autoencoder=Autoencoder()
-
-    print("Training model ...")
-    train_losses, valid_losses = train(nb_epoch, train_data, valid_data, my_autoencoder)
-
-    plot_Losses_Curve(train_losses,valid_losses)
-
-    save(my_autoencoder, saving_path)
-    save_decoder(my_autoencoder, saving_path_decoder)
-    save_encoder(my_autoencoder, saving_path_encoder)
-
-    return my_autoencoder
+#
+# def training_Saving_Autoencoder(nb_epoch, batchsize, dataset_path, saving_path, saving_path_decoder,saving_path_encoder):
+#     """Function that creates an autoencoder, trains it, and saves it to a file
+#         Args:
+#             nb_epoch (int): number of epoch to train the model with
+#             batchsize (int): batch size of data
+#             saving_path (str): path to the file in which the autoencoder is saved
+#             dataset_path (str): path to a directory containing the data as images
+#             saving_path_decoder (str): path to the file in which the decoder is saved
+#             saving_path_encoder (str): path to the file in which the encoder is saved
+#         Returns:
+#             Autoencoder: the created and trained autoencoder
+#     """
+#     print("Importing data ...")
+#     train_data, valid_data = Data_import(dataset_path, batchsize)
+#
+#     my_autoencoder=Autoencoder()
+#
+#     print("Training model ...")
+#     train_losses, valid_losses = train(nb_epoch, train_data, valid_data, my_autoencoder)
+#
+#     plot_Losses_Curve(train_losses,valid_losses)
+#
+#     save(my_autoencoder, saving_path)
+#     save_decoder(my_autoencoder, saving_path_decoder)
+#     save_encoder(my_autoencoder, saving_path_encoder)
+#
+#     return my_autoencoder
 
 
 #########################
@@ -492,38 +490,38 @@ def decoding_Vector_to_Image(vector, decoder):
 if __name__ == "__main__":
 
     # TRAINING
-    a = 1
+    print("main")
     # epoch = 1
     # batch_size = 16
     # my_autoencoder = training_Saving_Autoencoder(epoch,
     #                                             batch_size,
-    #                                             'faces_bis',
+    #                                             'faces',
     #                                             "models/Autoencoder"+str(epoch)+"epochs_"+str(batch_size)+"batchsize.pt",
     #                                             "models/Decoder"+str(epoch)+"epochs_"+str(batch_size)+"batchsize.pt",
     #                                             "models/Encoder"+str(epoch)+"epochs_"+str(batch_size)+"batchsize.pt")
-    # comparing_images(my_autoencoder,"faces_bis/00000/00002.png")
-    # comparing_images(my_autoencoder,"faces_bis/00000/00011.png")
-    # comparing_images(my_autoencoder,"faces_bis/00000/00020.png")
+    # comparing_images(my_autoencoder,"faces/1.png")
+    # comparing_images(my_autoencoder,"faces/2.png")
+    # comparing_images(my_autoencoder,"faces/3.png")
 
 
     # LOADING ENCODER & DECODER SEPARATELY
 
-    # loaded_decoder=load_decoder("models/BIS70000decoder_31_03_12epoch_16batchsize_64size_stride_padding.pt")
-    # loaded_encoder=load_encoder("models/3.pt")
+    loaded_encoder=load_encoder("models/encoder.pt")
+    loaded_decoder=load_decoder("models/decoder.pt")
 
-    # decoding_images(loaded_encoder,loaded_decoder,"faces_bis/00000/00002.png")
-    # decoding_images(loaded_encoder,loaded_decoder,"faces_bis/00000/00011.png")
-    # decoding_images(loaded_encoder,loaded_decoder,"faces_bis/00000/00020.png")
+    decoding_images(loaded_encoder,loaded_decoder,"faces/1.png")
+    # decoding_images(loaded_encoder,loaded_decoder,"faces/2.png")
+    # decoding_images(loaded_encoder,loaded_decoder,"faces/3.png")
 
 
     # LOADING AUTOENCODER
 
-    # my_autoencoder_loaded=load_autoencoder("models/BIS7000autoencoder_31_03_12epoch_16batchsize_64size_stride_padding.pt")
-    # comparing_images(my_autoencoder,"faces_bis/00000/00002.png")
-    # comparing_images(my_autoencoder,"faces_bis/00000/00011.png")
-    # comparing_images(my_autoencoder,"faces_bis/00000/00020.png")
+    # my_autoencoder_loaded=load_autoencoder("models/autoencoder.pt")
+    # comparing_images(my_autoencoder,"faces/1.png")
+    # comparing_images(my_autoencoder,"faces/2.png")
+    # comparing_images(my_autoencoder,"faces/3.png")
 
 
     # COMPUTING STD OF ENCODED VECTORS
 
-    # compute_Std_Per_Position_In_Encoded_Vectors(loaded_encoder, 'faces_bis')
+    # compute_Std_Per_Position_In_Encoded_Vectors(loaded_encoder, 'faces')
